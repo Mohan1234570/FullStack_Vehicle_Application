9cff293c5e2fc1508ea710ddc232c71f9055d7ca
GMK <mk4400320@gmail.com>
2024-05-19 18:53:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react';
+import { createBrowserRouter,  RouterProvider } from 'react-router-dom';
+//import SideBar from './components/SideBar';
+import Home from './components/Home';
+import ScenarioForm from './components/ScenarioForm';
+import ScenariosList from './components/ScenariosList';
+import VehicleForm from './components/VehicleForm';
+import './App.css'
+import Simulation from './components/Simulation';
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home />
+    },
+    {
+      path: 'add-vehicle',
+      element: <VehicleForm />
+    },
+    {
+      path: 'add-scenario',
+      element: <ScenarioForm />
+    },
+    {
+      path: 'all-scenarios',
+      element: <ScenariosList />
+    },
+    {
+      path: 'add-simulation',
+      element: <Simulation />
+    }
+  ])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        
+          <RouterProvider router={router} />
+      
+      </div>
+   
   );
 }
 
 export default App;
+
